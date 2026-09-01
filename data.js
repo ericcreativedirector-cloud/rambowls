ebf5ec1c8eb02811329c255d1f1f273868564526
Eric Rodriguez <254364328+ericcreativedirector-cloud@users.noreply.github.com>
2026-09-01 13:31:31 -0400

    Wk3: Ken back to out

--- data.js
+++ data.js
@@ -113,8 +113,8 @@ const NIGHTS = {
     played: false,
     in:    [["Eric",""], ["Phil",""], ["Ron",""]],
     maybe: [["Kelvyn","alt"]],
-    out:   [["Jamiqve","alt"]],
-    quiet: [["Ken",""], ["Michael",""], ["Prah",""], ["Pete","alt"]]
+    out:   [["Ken",""], ["Jamiqve","alt"]],
+    quiet: [["Michael",""], ["Prah",""], ["Pete","alt"]]
   }
 };
 
