195937af9a086ad47e227295f2fbdeeadbbeb84f
Eric Rodriguez <254364328+ericcreativedirector-cloud@users.noreply.github.com>
2026-09-16 21:14:49 -0400

    Week 5 game 1: lost to Bowls on Parade 540-577 (Ron 136, Phil 139, Ken 139)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Rambowls Scheduler - offline cache
 // Bump CACHE when you push an update, so phones pick up the new version.
-const CACHE = 'rambowls-v60';
+const CACHE = 'rambowls-v61';
 
 const ASSETS = [
   './',
